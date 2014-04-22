4abbe90f05c629f23c6efbafb7e92591927936ea
raz-varren <russ.vanderwaal+github@gmail.com>
2014-04-21 23:28:48 -0700

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,73 @@ js-chain-loader
 ===============
 
 Generate synchronous and asynchronous AJAX request queues.
+
+### Usage:
+
+##### JSFiddle: [http://jsfiddle.net/W9NfP/1/](http://jsfiddle.net/W9NfP/1/)
+
+```html
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Chain Loader</title>
+		<style type="text/css">
+			div{
+				float: left;
+				width: 150px;
+			}
+		</style>
+	</head>
+	<body>
+		<div>
+			<label for="sync">Synchronous</label>
+			<ul id="sync">
+
+			</ul>
+		</div>
+
+		<div>
+		<label for="async">Asynchronous</label>
+			<ul id="async">
+
+			</ul>
+		</div>
+		
+		<script type="text/javascript" src="jQuery-2.1.0.js"></script>
+		<script type="text/javascript" src="js-chain-loader.js"></script>
+		<script type="text/javascript">
+			var 
+				node = function(str){
+					return $('<li />').text(str);
+				},
+				makeRequest = function(id, callback){
+					return { 
+						request:{
+							url: '/echo/json/', 
+							data: {
+								json: JSON.stringify({id: id})
+							}
+						}, 
+						callback: callback
+					};
+				},
+				addSyncNode = function(data){ $('#sync').append(node('id: '+data.id)); },
+				addAsyncNode = function(data){ $('#async').append(node('id: '+data.id)); };
+
+			for(var i=0; i<10; i++){
+				ChainLoader.addSync(makeRequest(i, addSyncNode));
+				ChainLoader.addAsync(makeRequest(i, addAsyncNode));    
+			}
+
+			ChainLoader.flushSync()
+			.then(function(){
+				$('#sync').append(node('Sync Done'));
+				return ChainLoader.flushAsync();
+			})
+			.then(function(){
+				$('#async').append(node('Async Done'));
+			});
+		</script>
+	</body>
+</html>
+```
---
+++ js-chain-loader.js
@@ -0,0 +1,129 @@
+(function($){
+	window.ChainLoader = window.ChainLoader || function(){
+		'use strict';
+		
+		var
+		
+			debug = false,
+			
+			syncList = [],
+			
+			asyncList = [],
+			
+			ajaxDump = function(data){
+				log(data);
+			},
+			
+			ajaxError = function(){
+				log('ajax request failed:', arguments[0], arguments[1], arguments[2]);
+			},
+			
+			log = function(){
+				if(debug) console.log.apply(console, arguments);
+			},
+			
+			ajaxDefaults = {
+				url: '',
+				dataType: 'json',
+				type: 'post',
+				data: {},
+				error: ajaxError
+			},
+			
+			ajax = function(options) {
+				var options = options || {};
+				
+				for(var key in ajaxDefaults){
+					if(ajaxDefaults.hasOwnProperty(key)){
+						options[key] = options[key] || ajaxDefaults[key];
+					}
+				}
+				
+				return $.ajax(options);
+			},
+			
+			addSync = function(task){
+				syncList.push(task);
+			},
+			
+			addAsync = function(task){
+				asyncList.push(task);
+			},
+			
+			flushSync = function(){
+				var list = syncList;
+				syncList = [];
+				
+				if(list.length === 0) return $.Deferred().resolve();
+				
+				return synchronous(list);
+			},
+			
+			flushAsync = function(){
+				var list = asyncList;
+				asyncList = [];
+				
+				if(list.length === 0) return $.Deferred().resolve();
+				
+				return asynchronous(list);
+			},
+			
+			flush = function(){
+				
+				return flushSync()
+				.then(function(){
+					return flushAsync();
+				});
+			},
+			
+			synchronous = function(taskList, taskIdx){
+				taskIdx = taskIdx || 0;
+				
+				if(!taskList[taskIdx]) return;
+				return ajax(taskList[taskIdx].request)
+					.then(taskList[taskIdx].callback)
+					.then(function(){
+						return synchronous(taskList, taskIdx + 1);
+					});
+			},
+			
+			asynchronous = function(taskList){
+				var 
+					def = $.Deferred(),
+					tasksDone = 0, 
+					taskCount = taskList.length, 
+					task;
+				
+				for(var i = 0; i<taskCount; i++){
+					task = taskList[i];
+					
+					ajax(task.request).then(task.callback).then(function(){
+						tasksDone++;
+						if(tasksDone >= taskCount){
+							def.resolve();
+						}
+					});
+				}
+				
+				return def;
+			};
+		
+		//debug will become a different variable if this
+		//code is compiled, which will silence any logs
+		if(String(log).search('debug') > 0){ debug = true; }
+		log('ChainLoader loaded!');
+		
+		return {
+			sync: synchronous,
+			async: asynchronous,
+			addSync: addSync,
+			addAsync: addAsync,
+			flushSync: flushSync,
+			flushAsync: flushAsync,
+			flush: flush,
+			ajax: ajax,
+			log: log
+		};
+	}();
+	
+})(jQuery);
